bd5ddb0378b56f5464926445d6316c41cb8c9651
01001010sedano <janessedano@gmail.com>
2025-06-07 12:38:55 +0800

    Refactor: use environment variables for Firebase config, remove hardcoded secrets

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,14 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app"
 import { getFirestore } from "firebase-admin/firestore"
 import { getAuth } from "firebase-admin/auth"
-import { adminConfig } from "./firebase-config"
+
+const adminConfig = {
+  type: process.env.FIREBASE_ADMIN_TYPE,
+  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+}
 
 // Initialize Firebase Admin
 let adminApp
